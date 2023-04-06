95094a0278580d757882d2a9806b97a80b450c79
yezhonghu0503 <211736726@qq.com>
2023-04-06 20:52:38 +0800

    fix:生产环境配置

--- config/vite.config.prod.ts
+++ config/vite.config.prod.ts
@@ -1,9 +1,26 @@
 import { mergeConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import baseConig from './vite.config.base';
 
 export default mergeConfig(
   {
     mode: 'production',
+    server: {
+      open: true,
+      fs: {
+        strict: true,
+      },
+      proxy: {
+        '/admin': 'http://42.192.219.155:8081',
+      },
+    },
+    plugins: [
+      eslint({
+        cache: false,
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'src/**/*.vue'],
+        exclude: ['node_modules'],
+      }),
+    ],
   },
   baseConig
 );
